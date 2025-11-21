97bfe1c6abd64322015474f916f1f3d5a8577097
jmjules <126357309+jmjules@users.noreply.github.com>
2025-11-21 22:49:42 +0100

    update overlay nonreactive

--- src/components/Overlay.vue
+++ src/components/Overlay.vue
@@ -1,13 +1,25 @@
 <template>
   <div class="overlay">
-    <button @click="onCamera(0)">Camera 1</button>
-    <button @click="onCamera(1)">Camera 2</button>
-    <button @click="onCamera(2)">Camera 3</button>
+    <div class="title">Title text</div>
+    <div class="body-text">Here is some body text</div>
+    <div>
+      <button @click="emit('previous')">Back</button>
+      <button @click="emit('next')">Next</button>
+    </div>
+    <br>
+    <br>
+    <br>
+    <hr>
+    <div>
+      <button @click="onCamera(0)">Camera 1</button>
+      <button @click="onCamera(1)">Camera 2</button>
+      <button @click="onCamera(2)">Camera 3</button>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-const emit = defineEmits(["switch-camera"]);
+const emit = defineEmits(["switch-camera", "previous", "next"]);
 
 function onCamera(i: number) {
   emit("switch-camera", i);
@@ -20,7 +32,16 @@ function onCamera(i: number) {
   top: 20px;
   left: 20px;
   display: flex;
+  flex-direction: column;
   gap: 10px;
   z-index: 100;
+  padding: 1rem;
+
+  background-color: blanchedalmond;
+}
+
+.title {
+  font-size: larger;
+  font-weight: bold;
 }
 </style>
